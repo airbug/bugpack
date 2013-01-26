71f9cbcb4e46e3e4bf7143aadd26b34606b2ce22
Brian Neisler <neisler.brian@gmail.com>
2013-01-26 11:45:40 -0800

    [Imp] update build script to have a local and a production build flow.

--- buildbug.js
+++ buildbug.js
@@ -66,9 +66,17 @@ buildProperties({
 // Declare Flows
 //-------------------------------------------------------------------------------
 
+
+// Clean Flow
+//-------------------------------------------------------------------------------
+
 buildTarget("clean").buildFlow(
     targetTask("clean")
 );
+
+
+// Local Flow
+//-------------------------------------------------------------------------------
 
 buildTarget("local").buildFlow(
 
@@ -82,25 +90,25 @@ buildTarget("local").buildFlow(
             series([
                 targetTask("createNodePackage", {
                     properties: {
-                        packageJson: buildProject.getProperties().bugpackNode.packageJson,
-                        sourcePaths: buildProject.getProperties().bugpackNode.sourcePaths
+                        packageJson: buildProject.getProperty("bugpackNode.packageJson"),
+                        sourcePaths: buildProject.getProperty("bugpackNode.sourcePaths")
                     }
                 }),
                 targetTask("packNodePackage", {
                     properties: {
-                        packageName: buildProject.getProperties().bugpackNode.packageJson.name,
-                        packageVersion: buildProject.getProperties().bugpackNode.packageJson.version
+                        packageName: buildProject.getProperty("bugpackNode.packageJson.name"),
+                        packageVersion: buildProject.getProperty("bugpackNode.packageJson.version")
                     }
                 }),
                 targetTask("s3EnsureBucket", {
                     properties: {
-                        bucket: "node_modules"
+                        bucket: buildProject.getProperty("local-bucket")
                     }
                 }),
                 targetTask("s3PutFile", {
                     init: function(task, buildProject, properties) {
-                        var packedNodePackage = nodejs.findPackedNodePackage(buildProject.getProperties().bugpackNode.packageJson.name,
-                            buildProject.getProperties().bugpackNode.packageJson.version);
+                        var packedNodePackage = nodejs.findPackedNodePackage(buildProject.getProperty("bugpackNode.packageJson.name"),
+                            buildProject.getProperty("bugpackNode.packageJson.version"));
                         task.updateProperties({
                             file: packedNodePackage.getFilePath(),
                             options: {
@@ -109,32 +117,32 @@ buildTarget("local").buildFlow(
                         });
                     },
                     properties: {
-                        bucket: "node_modules"
+                        bucket: buildProject.getProperty("local-bucket")
                     }
                 })
             ]),
             series([
                 targetTask("createNodePackage", {
                     properties: {
-                        packageJson: buildProject.getProperties().bugpackRegistry.packageJson,
-                        sourcePaths: buildProject.getProperties().bugpackRegistry.sourcePaths
+                        packageJson: buildProject.getProperty("bugpackRegistry.packageJson"),
+                        sourcePaths: buildProject.getProperty("bugpackRegistry.sourcePaths")
                     }
                 }),
                 targetTask("packNodePackage", {
                     properties: {
-                        packageName: buildProject.getProperties().bugpackRegistry.packageJson.name,
-                        packageVersion: buildProject.getProperties().bugpackRegistry.packageJson.version
+                        packageName: buildProject.getProperty("bugpackRegistry.packageJson.name"),
+                        packageVersion: buildProject.getProperty("bugpackRegistry.packageJson.version")
                     }
                 }),
                 targetTask("s3EnsureBucket", {
                     properties: {
-                        bucket: "node_modules"
+                        bucket: buildProject.getProperty("local-bucket")
                     }
                 }),
                 targetTask("s3PutFile", {
                     init: function(task, buildProject, properties) {
-                        var packedNodePackage = nodejs.findPackedNodePackage(buildProject.getProperties().bugpackRegistry.packageJson.name,
-                            buildProject.getProperties().bugpackRegistry.packageJson.version);
+                        var packedNodePackage = nodejs.findPackedNodePackage(buildProject.getProperty("bugpackRegistry.packageJson.name"),
+                            buildProject.getProperty("bugpackRegistry.packageJson.version"));
                         task.updateProperties({
                             file: packedNodePackage.getFilePath(),
                             options: {
@@ -143,10 +151,96 @@ buildTarget("local").buildFlow(
                         });
                     },
                     properties: {
-                        bucket: "node_modules"
+                        bucket: buildProject.getProperty("local-bucket")
                     }
                 })
             ])
         ])
     ])
 ).makeDefault();
+
+
+// Prod Flow
+//-------------------------------------------------------------------------------
+
+buildTarget("prod").buildFlow(
+
+    series([
+
+        // TODO BRN: This "clean" task is temporary until we"re not modifying the build so much. This also ensures that
+        // old source files are removed. We should figure out a better way of doing that.
+
+        targetTask("clean"),
+        parallel([
+            series([
+                targetTask("createNodePackage", {
+                    properties: {
+                        packageJson: buildProject.getProperty("bugpackNode.packageJson"),
+                        sourcePaths: buildProject.getProperty("bugpackNode.sourcePaths")
+                    }
+                }),
+                targetTask("packNodePackage", {
+                    properties: {
+                        packageName: buildProject.getProperty("bugpackNode.packageJson.name"),
+                        packageVersion: buildProject.getProperty("bugpackNode.packageJson.version")
+                    }
+                }),
+                targetTask("s3EnsureBucket", {
+                    properties: {
+                        bucket: "airbug"
+                    }
+                }),
+                targetTask("s3PutFile", {
+                    init: function(task, buildProject, properties) {
+                        var packedNodePackage = nodejs.findPackedNodePackage(buildProject.getProperty("bugpackNode.packageJson.name"),
+                            buildProject.getProperty("bugpackNode.packageJson.version"));
+                        task.updateProperties({
+                            file: packedNodePackage.getFilePath(),
+                            options: {
+                                ACL: 'public-read'
+                            }
+                        });
+                    },
+                    properties: {
+                        bucket: "airbug"
+                    }
+                })
+            ]),
+            series([
+                targetTask("createNodePackage", {
+                    properties: {
+                        packageJson: buildProject.getProperty("bugpackRegistry.packageJson"),
+                        sourcePaths: buildProject.getProperty("bugpackRegistry.sourcePaths")
+                    }
+                }),
+                targetTask("packNodePackage", {
+                    properties: {
+                        packageName: buildProject.getProperty("bugpackRegistry.packageJson.name"),
+                        packageVersion: buildProject.getProperty("bugpackRegistry.packageJson.version")
+                    }
+                }),
+                targetTask("s3EnsureBucket", {
+                    properties: {
+                        bucket: "airbug"
+                    }
+                }),
+                targetTask("s3PutFile", {
+                    init: function(task, buildProject, properties) {
+                        var packedNodePackage = nodejs.findPackedNodePackage(buildProject.getProperty("bugpackRegistry.packageJson.name"),
+                            buildProject.getProperty("bugpackRegistry.packageJson.version"));
+                        task.updateProperties({
+                            file: packedNodePackage.getFilePath(),
+                            options: {
+                                ACL: 'public-read'
+                            }
+                        });
+                    },
+                    properties: {
+                        bucket: "airbug"
+                    }
+                })
+            ])
+        ])
+    ])
+);
+
